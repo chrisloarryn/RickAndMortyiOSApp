54178a783b598e43ecb75ca517dbd7238982464a
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-08-13 01:42:16 -0400

    feat: add request handler and model for requests

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Cristóbal Contreras on 13-08-23.
+//
+
+import Foundation
+
+/// Object that represents a singlet API call
+final class RMRequest {
+    
+}
